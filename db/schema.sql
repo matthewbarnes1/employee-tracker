c337ad177ad3eeea7f2509b9c80457a2b2dce349
matthewbarnes1 <matthew.barnes35@gmail.com>
2023-09-18 21:38:59 -0400

    working on schema sql

--- db/schema.sql
+++ db/schema.sql
@@ -4,30 +4,31 @@ CREATE DATABASE employee_db;
 USE employee_db;
 
 CREATE TABLE department (
-  id INT NOT NULL,
-  name: VARCHAR(30) to hold department name,
-  course_title VARCHAR(30) NOT NULL,
-  course_description TEXT NOT NULL,
+  id INT NOT NULL PRIMARY KEY,
+  name VARCHAR(30) NOT NULL,
   active BOOLEAN NOT NULL,
   date_added DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
 
+CREATE TABLE role (
+  id INT NOT NULL PRIMARY KEY,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL(10, 2) NOT NULL,
+  department_id INT,
+  date_updated DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
+  FOREIGN KEY (department_id) REFERENCES department(id)
+);
+
 CREATE TABLE employee (
-  id: INT PRIMARY KEY,
-  role_id: INT to hold reference to employee role,
-  manager_id: INT to hold reference to another employee that is the manager of the current employee (null if the employee has no manager),
+  id INT NOT NULL PRIMARY KEY,
+  role_id INT,
+  manager_id INT,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
   active BOOLEAN NOT NULL,
-  date_updated DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL
-);
-
-CREATE TABLE role (
-  id: INT PRIMARY KEY,
-title: VARCHAR(30) to hold role title,
-salary: DECIMAL to hold role salary,
-department_id: INT to hold reference to department role belongs to,
-  date_updated DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL
+  date_updated DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
+  FOREIGN KEY (role_id) REFERENCES role(id),
+  FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
 
 
